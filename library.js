bd4061173f45fb04424260c0988ef83e1b842759
Aziz Khoury <bentael@gmail.com>
2013-11-11 09:57:48 -0500

    wip

--- library.js
+++ library.js
@@ -47,14 +47,63 @@ module.exports = {
         });
     },
 
+    writeJSONtoFile: function(){
+
+    },
+
+    throttleSelectQuery: function(columnsString, table, options) {
+        options = options || {};
+        options.limit = options.limit || 1000;
+        options.queryCallback = options.queryCallback || function(){};
+
+        var self = this;
+
+        var total = 0;
+        this.ubbq("SELECT COUNT(*) as total FROM " + table, function(err, rows){
+
+            if (rows.length)
+                total = rows[0]['total'] || 0;
+
+            var funcs = [];
+
+            var createfunc = function (i, total) {
+                return function(a, b, c) {
+                    return options.queryCallback(a, b, c, i >= total);
+                };
+            };
+
+            for (var i = options.limit; i < total + options.limit; ) {
+                funcs[i] = createfunc(i, total);
+                i += options.limit;
+            }
+
+            console.log("TOTAL: " + total);
+            for (var j = options.limit; j < total + options.limit; ) {
+                var q = "SELECT " + columnsString + " FROM " + table + " LIMIT " + (j - options.limit) + ", "+ options.limit;
+                // console.log(q);
+                self.ubbq(q, funcs[j]);
+                j += options.limit;
+            }
+        });
+    },
+
     // get ubb users
     ubbGetUsers: function() {
         var users = [];
-        var self = this;
-
-        this.ubbq("", function(){
-
-        });
+        this.throttleSelectQuery(
+            // select
+            "USER_ID as id, USER_LOGIN_NAME as username, USER_REGISTRATION_EMAIL as email, USER_IS_APPROVED as approved, USER_IS_banned as banned",
+            // from
+            "ubbt_USERS",
+            {
+                queryCallback: function(err, rows, fields, lastOne){
+                    if (err) throw err;
+                    users = users.concat(rows);
+                    if (lastOne)
+                        console.log(users.length);
+                }
+            }
+        );
     },
 
     ubbGetBannedUsers: function(cb) {
@@ -82,15 +131,5 @@ module.exports = {
     getUBBTopics: function() {},
 
     // get ubb posts
-    getUBBPosts: function() {},
-
-    hashPassword: function(password, saltRounds) {
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            console.log(saltRounds + " salt: " + salt);
-            bcrypt.hash(password, salt, function(err, pwd){
-                if (err) throw err;
-                console.log("saltRounds: " + saltRounds + ", " + password +  " --> " + pwd);
-            });
-        });
-    }
+    getUBBPosts: function() {}
 };
--- temp.js
+++ temp.js
@@ -1,5 +1,3 @@
 var ubb = require("./library.js");
 
-for(var i =0; i < 12; i++){
-    ubb.hashPassword("5QvoUb!3Xq", 12);
-}
+ubb.ubbGetUsers();
